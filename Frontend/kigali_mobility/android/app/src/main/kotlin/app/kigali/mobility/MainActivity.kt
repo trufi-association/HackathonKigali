ba0329736abce0d0bc15108ba933a396ded98f79
Gustavo T <tgustavoch@gmail.com>
2024-10-22 16:17:18 -0400

    [Feat] Configure android-platform for flutter_v3.24.3

--- Frontend/kigali_mobility/android/app/src/main/kotlin/app/kigali/mobility/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package app.kigali.mobility
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ Frontend/kigali_mobility/android/app/src/main/kotlin/com/example/kigali_mobility/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kigali_mobility
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
