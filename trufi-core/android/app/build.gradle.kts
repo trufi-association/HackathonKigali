246967c94f497b5115ced56959ba81fd61e1eb15
Gustavo T <tgustavoch@gmail.com>
2025-08-04 00:37:13 -0400

    [Feat] Update ndkVersion to 28.1.13356709 for maplibre_gl compatibility

--- trufi-core/android/app/build.gradle.kts
+++ trufi-core/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.trufi_core"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.1.13356709"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
